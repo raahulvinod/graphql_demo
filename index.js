4808c5af2f9a1d4035cf4a8d92b18d9a327dae4a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-03 15:34:47 +0530

    feat: add schema and types

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+import { typeDefs } from './schema';
+
 // server setup
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs, // definitions of types of datas
   resolvers,
 });
 
